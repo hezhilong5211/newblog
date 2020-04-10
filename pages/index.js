03029649137d77f45190a725a4892962f8ae16db
hezhilong1 <hezhilong1@100tal.com>
2020-04-10 16:09:54 +0800

    框架搭建完毕

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import React from 'react'
+import { Button } from 'antd'
 
 const Home = () => (
   <div className="container">
     <Head>
       <title>Home</title>
+      <div><Button type="primary">nihao</Button></div>
     </Head>
   </div>
 )
